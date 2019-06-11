1a45a9841a9ccf1998a634bc369d469fdea4b2e0
Dominic Brailsford <dbrailsf@fnal.gov>
2019-06-11 12:29:12 -0500

    Just about make the metadata json file. Need to decide what/how to use data_tier and file_format (as its a fcl file)

--- scripts/poms/sbndpoms_genfclwithrunnumber_maker.sh
+++ scripts/poms/sbndpoms_genfclwithrunnumber_maker.sh
@@ -4,6 +4,7 @@
 #Subrun follows $PROCESS+1
 #After every 100 subruns, a new run is started
 export NSUBRUNSPERRUN=100
+export WORKDIR="/sbnd/app/users/dbrailsf/generated_fcls"
 
 while :; do
     case $1 in
@@ -41,6 +42,15 @@ while :; do
                 exit 1
             fi
             ;;
+        --workdir)       # Takes an option argument; ensure it has been specified.
+            if [ "$2" ]; then
+                WORKDIR="$2"
+                shift
+            else
+                echo "$0 ERROR: workdir requires a non-empty option argument."
+                exit 1
+            fi
+            ;;
 
         -v|--verbose)
             verbose=$((verbose + 1))  # Each -v adds 1 to verbosity.
@@ -71,18 +81,88 @@ if [ -z "$NFILES" ]; then
   exit 2
 fi
 
+#make the output direcrory
+mkdir -p $OUTDIR
+mkdir -p $WORKDIR
 
-##We need to extract what the default run number is.  Let's get this from running lar and dumping the config
-#lar --debug-config lar_config_dump.txt -c $FCL
-#DEFAULTRUN=`grep -r "firstRun" lar_config_dump.txt`
-##THis line is of the form firstRun: RUN
-##We need to extract RUN
-##Delimit on the colon
-#IFS=\: read -a DEFAULTRUNARRAY <<<"$DEFAULTRUN"
-##Extract the run number
-#DEFAULTRUN=${DEFAULTRUNARRAY[1]}
-##Get the subrun candidate number.  We will need to do some modulo arithmetic to work out the actual run and subrun numbers
-#let "SUBRUNCANDIDATE= $PROCESS +1"
+#We need to extract what the default run number is.  Let's get this from running lar and dumping the config
+lar --debug-config lar_config_dump.txt -c $FCL
+DEFAULTRUN=`grep -r "firstRun" lar_config_dump.txt`
+#THis line is of the form firstRun: RUN
+#We need to extract RUN
+#Delimit on the colon
+IFS=\: read -a DEFAULTRUNARRAY <<<"$DEFAULTRUN"
+#Extract the run number
+DEFAULTRUN=${DEFAULTRUNARRAY[1]}
+
+#starting making the files
+NPROCESSEDFILES=0
+#Needed to make subruns always start at 1
+let "SUBRUNCANDIDATE= 0"
+while [[ $NPROCESSEDFILES -lt $NFILES ]]
+do
+#Do arithmetic to modulo-ise the run number and subrun number whilst making sure subrun always starts at 1
+  let "SUBRUNCANDIDATE++"
+  RUNINCREMENT=`python -c "print $SUBRUNCANDIDATE//$NSUBRUNSPERRUN"`
+  let "RUNNUMBER= $DEFAULTRUN + $RUNINCREMENT"
+  SUBRUNNUMBER=`python -c "print $SUBRUNCANDIDATE % $NSUBRUNSPERRUN"`
+#Never let subrun be 0
+  if [[ $SUBRUNNUMBER -eq 0 ]]
+  then
+   continue
+  fi
+#We now have  run and subrun number
+#make the fcl
+  OUTFCL="${FCL%.*}_run${RUNNUMBER}_subrun${SUBRUNNUMBER}.fcl"
+  if [ -e $WORKDIR/$OUTFCL ]
+  then
+    rm $WORKDIR/$OUTFCL
+  fi
+  echo "Producing $WORKDIR/$OUTFCL"
+  echo -e "#include \"$FCL\"\n" \
+          "source.firstRun: $RUNNUMBER\n" \
+          "source.firstSubRun: $SUBRUNNUMBER" >> $WORKDIR/$OUTFCL
+  ifdh addOutputFile $WORKDIR/$OUTFCL
+  ifdh renameOutput unique
+  UNIQUEOUTFCL=`find $WORKDIR -name ${OUTFCL%.*}*`
+  UNIQUEOUTFCL=`basename $UNIQUEOUTFCL`
+  ifdh copyBackOutput $OUTDIR
+  ifdh cleanup
+
+echo $name
+
+  #Now make a json
+  echo -e "
+  {
+      \"file_type\": \"mc\",
+      \"group\": \"sbnd\",
+      \"fcl.name\": \"$FCL\",
+      \"sbnd_project.name\": \"${FCL%.*}\",
+      \"sbnd_project.stage\": \"fcl\",
+      \"sbnd_project.version\": \"v06_70_01_01_SBNWorkshop0318\",
+      \"sbnd_project.software\": \"sbndcode\",
+      \"production.name\": \"SBNWorkshop0318\",
+      \"production.type\": \"workshop\",
+      \"data_tier\": \"fcl\",
+      \"file_format\": \"fcl\",
+      \"start_time\": \"`date +"%FT%T"`\",
+      \"end_time\": \"`date +"%FT%T"`\",
+      \"runs\": [ [ $RUNNUMBER, $SUBRUNNUMBER, \"physics\" ] ],
+      \"event_count\": 0,
+  \"first_event\":  -9999 ,
+  \"last_event\":  -9999 ,
+  \"group\": \"sbnd\",
+    \"file_name\": \"$UNIQUEOUTFCL\",
+    \"file_size\": \"`wc -c < $WORKDIR/$UNIQUEOUTFCL`\",
+    \"checksum\": 
+      `ifdh checksum $WORKDIR/$UNIQUEOUTFCL`
+    
+  }
+  " >> $WORKDIR/${UNIQUEOUTFCL}.json
+  let "NPROCESSEDFILES++"
+done
+  echo $name
+
 ##Calculate how much to increment the run by
 #RUNINCREMENT=`python -c "print $SUBRUNCANDIDATE//$NSUBRUNSPERRUN"`
 ##Calculate the run number
