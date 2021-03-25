c6afba7ce3901372657e9faa2ec75c1d5eb00435
Edward Tyley <e.tyley@sheffield.ac.uk>
2021-03-25 05:18:29 -0500

    Use sed to append metadata json files so that it can run on files stored on dCache

--- scripts/poms/sbndpoms_metadata_extractor.sh
+++ scripts/poms/sbndpoms_metadata_extractor.sh
@@ -15,24 +15,23 @@ sed -i '$ d' $JSONFILE
 #We also need to add a comma to the end of the last line now that we are going to add more line
 sed -i '$ s/$/\,/g' $JSONFILE
 
-
 #Add the filename back in
-echo "\"file_name\": \"`basename $INPUTFILE`\"," >> $JSONFILE
+sed -i "\$a \"file_name\": \"$(basename $INPUTFILE)\"," $JSONFILE
 #Add the file size
 FILESIZE=`wc -c < $INPUTFILE`
-echo "\"file_size\": \"$FILESIZE\"," >> $JSONFILE
+sed -i "\$a \"file_size\": \"$FILESIZE\"," $JSONFILE
 
 
 #calculate checksum
 CHECKSUM=`samweb -e sbnd file-checksum $INPUTFILE`
-echo "\"checksum\": $CHECKSUM," >> $JSONFILE
+sed -i "\$a \"checksum\": $CHECKSUM," $JSONFILE
 
 
 #Make sure that the last entry does not have a comma at the end of its line
 sed -i '$ s/,//g' $JSONFILE
 #
 ##re-add the closing brace
-echo "}" >> $JSONFILE
+sed -i '$a }' $JSONFILE
 
 #Dump the json to std::out, needed for metadata_extractor
 cat $JSONFILE
